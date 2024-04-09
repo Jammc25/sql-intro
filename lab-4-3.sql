2f4407299088c1e1b1777a8be03f97f4ce3bde0f
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-04-09 23:54:15 +0000

    Completed part 4 questions!

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -7,3 +7,8 @@
 -- +---------------+------------+-----------+-----------+
 
 
+SELECT teams.name, players.first_name, players.last_name, MAX(stats.home_runs)
+FROM teams INNER JOIN stats ON teams.id = stats.team_id
+INNER jOIN players ON players.id = stats.player_id
+WHERE teams.year = 2019
+;
